d58c9c2be750f32bd2c871ee424fbae125e7c342
Oliver Smith <ollieparanoid@postmarketos.org>
2020-03-21 16:44:34 +0100

    .ci/generate_page.sh: fix check-passed filename

--- .ci/generate_page.sh
+++ .ci/generate_page.sh
@@ -2,7 +2,7 @@
 
 mkdir -p public
 
-if [ -e .upstream-compat.check-passed ]; then
+if [ -e upstream-compat.check-passed ]; then
 	cp badges/upstream-compat-passed.svg public/upstream-compat.svg
 else
 	cp badges/upstream-compat-failed.svg public/upstream-compat.svg
